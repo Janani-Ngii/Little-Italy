2b5d2735bbcd0e0a408c08c620a1c739add8c259
Janani-Ngii <janongii012@gmail.com>
2022-03-20 20:52:29 +0300

    adding js functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function(){}
 
-    let value = document.getElementById("delivery").value;
-    if (value == "delivery") {
-       console("Input a delivery location");
-        }
+function Order(Size, Crust, Toppings){
+    this.Size= Size;
+    this.Crust = Crust;
+    this.Toppings = Toppings;
+    this. number = number;
+}
+
+var size = document.getElementById("size").val
+var Toppings= document.getElementById("Toppings").val
+var Crust = document.getElementById("Crust").val
+var number =document.getElementById("number").val
+
+$("#checkout").click(function){
+    function mySize(){
+        var size = document.getElementById("size").val
+    }
+    function myCrust(){
+        var Crust = document.getElementById("Crust").val
+    }
+    function myToppings(){
+        var Toppings= document.getElementById("Toppings").val
+    }
+    function myNumber(){
+        var number =document.getElementById("number").val
+    }
+
+var person=new Order(myCrust(), myNumber(), mySize(), myToppings())
+
+})
+
+})
 
 
+
+
+   
         
-        })
+    
+    
+
+
+
+  
+})
