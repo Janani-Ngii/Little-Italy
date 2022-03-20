587c33c0103064c869f33f7dfdc4eb58c095aaf8
Janani-Ngii <janongii012@gmail.com>
2022-03-21 01:02:40 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
 $(document).ready(function(){}
 
 function Order(Size, Crust, Toppings){
-    this.Size= Size;
-    this.Crust = Crust;
-    this.Toppings = Toppings;
+    this.size= size;
+    this.crust = crust;
+    this.toppings = toppings;
     this. number = number;
 }
 
 var size = document.getElementById("size").val
-var Toppings= document.getElementById("Toppings").val
-var Crust = document.getElementById("Crust").val
+var toppings= document.getElementById("Toppings").val
+var crust = document.getElementById("Crust").val
 var number =document.getElementById("number").val
 
-$("#checkout").click(function){
+$("#checkout").click(function)();{
     function mySize(){
         var size = document.getElementById("size").val
+        parseInt(size)
     }
     function myCrust(){
-        var Crust = document.getElementById("Crust").val
+        var crust = document.getElementById("crust").val
+        parseInt(crust)
     }
     function myToppings(){
-        var Toppings= document.getElementById("Toppings").val
+        var toppings= document.getElementById("toppings").val
+        parseInt(toppings)
     }
     function myNumber(){
         var number =document.getElementById("number").val
     }
 
 var person=new Order(myCrust(), myNumber(), mySize(), myToppings())
+total = (myCrust+mysize+myToppings) * myNumber;
+var total =(person.size+person.toppings+person.crust)*(person.number);
 alert(total);
 });
 
@@ -43,4 +48,3 @@ alert(total);
 
 
   
-})
